42b7b3d7c0776475d4b366036423b79c4a513034
sam bacha <sam@manifoldfinance.com>
2024-02-26 08:53:31 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,3 +10,18 @@ fi
 
 set -euxo pipefail
 git submodule update --init --recursive --progress --depth=1 --checkout $NAMES
+
+# update submodule
+git submodule update --init
+# set tag for submodules
+DEFAULT_TAG=v1.0.0"
+# update
+git config -f .gitmodules --get-regexp '^submodule\..*\.url$' |
+    while read -r KEY MODULE_PATH; do
+        NAME="$(echo "${KEY}" | sed 's/^submodule\.\(.*\)\.url$/\1/')"
+        if [[ $NAME =~ "v1.0.0" ]]; then
+          pushd "${NAME}"
+          git checkout -b latest_tag_branch $DEFAULT_TAG                   
+          popd
+        fi
+    done
